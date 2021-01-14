d80be3e721cd987960388c15a6cf36c22e84d46b
NURU SYAHRI <76667386+syahricogan@users.noreply.github.com>
2021-01-14 17:35:08 +0700

    Add files via upload

---
+++ src/iklan.js
@@ -0,0 +1,22 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *DAFTAR SEWA BOT☂️ :*
+╠➥ *SEWA : 10K/GRUP (BULAN)*
+╠➥ *PEMBAYARAN BISA MELALUI :*
+╠➥ *PULSA,OVO*
+╠═══════════════════════════
+╠➥ *KEUNTUNGAN SEWA BOT :*
+╠➥ *1. BISA MENJADI ADMIN Rizky*
+╠➥ *2. BISA MENDAPATKAN COMMAND ADMIN*
+╠═══════════════════════════
+╠➥ *JIKA MINAT IKLAN DIATAS*
+╠➥ *HARAP HUBUNGI NOMOR DIBAWAH :*
+╠➥ *wa.me/681389368689*
+║
+╚═〘  Z - BOT 〙
+`
+}
+exports.iklan = iklan
---
+++ src/nsfwmenu.js
@@ -0,0 +1,20 @@
+const nsfwmenu = (prefix) => { 
+	return `
+╔══✪〘 NSFW 〙✪══
+║
+╠➥ *${prefix}hentai*
+╠➥ *${prefix}nsfwtrap*
+╠➥ *${prefix}nsfwneko*
+╠➥ *${prefix}loli*
+╠➥ *${prefix}nsfwloli*
+╠➥ *${prefix}randomanime*
+╠➥ *${prefix}cry*
+╠➥ *${prefix}kiss*
+╠➥ *${prefix}randomhug*
+╠➥ *${prefix}nekonime*
+╠➥ *${prefix}waifu*
+╠➥ *${prefix}waifu2*
+║
+╚═〘  Z - BOT 〙`
+}
+exports.nsfwmenu = nsfwmenu
